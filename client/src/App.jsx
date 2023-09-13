cfcfac00cf2e5ed37f55f157259b2d3bd26e987d
caraka wijaya <109886433+caraka20@users.noreply.github.com>
2023-09-13 22:16:12 +0700

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Routes>
+        {/* <Route /> */}
+      </Routes>
+    </>
+  )
+}
+
+export default App
+
