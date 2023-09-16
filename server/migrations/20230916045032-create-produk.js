a15f663b8a5f28b9a8c622b8e860a436d021ad00
Arief Rubani <hidayat.arief44@yahoo.com>
2023-09-16 13:57:21 +0700

    MP3-32

---
+++ server/migrations/20230916045032-create-produk.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('produks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nama_produk: {
+        type: Sequelize.STRING
+      },
+      deskripsi: {
+        type: Sequelize.STRING
+      },
+      stock: {
+        type: Sequelize.INTEGER
+      },
+      image_product: {
+        type: Sequelize.STRING
+      },
+      status_product: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('produks');
+  }
+};
