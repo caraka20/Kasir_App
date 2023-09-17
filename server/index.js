72e822f94e014790d78a867a3521380808bfd3f0
Arief Rubani <hidayat.arief44@yahoo.com>
2023-09-17 11:14:52 +0700

    updated controller - user login

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const app = express()
 const cors = require("cors")
+const { userRouter } = require("./routers")
 app.use(cors())
 app.use(express.json())
+app.use("/user",userRouter)
+// app.use("/user", userRouter)
 PORT = 3001
-
-
 
 app.listen(PORT, () => {
     console.log("Sedang Berjalan " + PORT);
