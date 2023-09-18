9ef1182cbae44c5384d0109969976a9d16803860
Arief Rubani <hidayat.arief44@yahoo.com>
2023-09-18 11:49:23 +0700

    multer belum selesai tapi

---
+++ server/controller/authorizationController.js
@@ -0,0 +1,11 @@
+const conn = require ("../models")
+
+module.exports = {
+    resetPassword: async (req, res, next) => {
+        try {
+            
+        } catch (error) {
+            next(error)
+        }
+    }
+}
--- server/controller/userController.js
+++ server/controller/userController.js
@@ -6,21 +6,54 @@ module.exports = { //udah bisa namun belum ada validasi samsek
             const {username, password} = req.query 
             const masuk = await conn.user.findOne({where: {username: username, password: password}});
             if(!masuk) {
-                res.status(401).send({
-                    message:"username or password is invalid!"
-                })
-            } else {
-                res.status(200).send({
-                    isError: false,
-                    message:"Welcome back, have beautiful day!",
-                    data: masuk
-                })
+                throw {
+                    isError: true,
+                    message: "Username or password is invalid!"
+                }
             }
+            // Validasi ketika user cashier itu berstatus tidak aktif
+            if(masuk && masuk.status_user == "inactive") { 
+                throw {
+                    isError: true,
+                    message: "You are currently deactivated, please contact admin"
+                }
+            }
+            res.status(200).send({
+                isError: false,
+                message:"Welcome back, have a pleasant day!",
+                data: masuk
+            })
             // const masuk = await conn.user.findOne()
         } catch (error) {
-            console.log(error.message)
+            next(error)
         }
     },
+    // update: async(req, res, next) => {
+    //     try{
+    //         //1. Ambil id image
+    //         const {idImage} = req.params
+    //         //2. Ambil path images
+    //         const findImage = await db.hotel_image.findOne({
+    //             where: {
+    //                 id:idImage
+    //             }
+    //         })
+    //         //3. Update new path on tabel
+    //         await db.hotel_image.update({url: req.files.images[0].path}, {where: {id: idImage}})
 
+    //         //4. Delete image lama
+    //         deleteFiles({images: [{path: findImage.dataValues.url}]}) // gini tulisnya karena bentuk objekt
+
+    //         //5. Kirim response
+    //         res.status(201).send({
+    //             isError: false,
+    //             message: "Image has been successfully updated!",
+    //             data: null
+    //         })
+    //     } catch (error) {
+    //         deleteFiles(req.files)
+    //         next(error)
+    //     }
+    // }
 }
 // ganbatte2023
