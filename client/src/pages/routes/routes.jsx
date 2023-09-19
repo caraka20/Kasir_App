f77fecbab1400a675d75ed3a51e41b9927499f5d
Anggoro10 <trianggoroi84@gmail.com>
2023-09-19 12:27:56 +0700

    set-up admin frond end

--- client/src/pages/routes/routes.jsx
+++ client/src/pages/routes/routes.jsx
@@ -1,10 +1,11 @@
 import { Route } from "react-router-dom";
-
+import CreatePorduk from "../CreateProduk/CreatePorduk";
 // PAGES
 import Home from "../Home/Home";
 
 const routes = [
   <>
+    <Route path="/produk" element={<CreatePorduk />}/>
     <Route path="/"  element={<Home/>}/>
   </>
   
