630db6a427823bf24865fc93b90c988be073d304
Anggoro10 <trianggoroi84@gmail.com>
2023-09-20 13:53:26 +0700

    backend produk done

--- server/routers/productRouter.js
+++ server/routers/productRouter.js
@@ -2,9 +2,11 @@ const express = require('express')
 const Router = express.Router()
 
 const {productController} = require('../controller')
+const upload = require('../middleware/upload')
 
-Router.post('/', productController.create)
+Router.post('/', upload, productController.create)
 Router.put('/:id', productController.update)
 Router.patch('/:id', productController.deleteStatus)
+Router.patch('/img/:idProduk', upload, productController.updateImageProduk)
 
 module.exports = Router
