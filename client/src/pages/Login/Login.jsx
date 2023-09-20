6f8f5b982bf16d6231c64e316c18a1213bf6fe93
Arief Rubani <hidayat.arief44@yahoo.com>
2023-09-20 19:43:19 +0700

    MP3-20 Frontend Login Done

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -30,7 +30,8 @@ const Login = () => {
       }
       
     }
-    console.log(state);
+    // console.log(state);
+    console.log(state)
       return (
         <div className='flex justify-center items-center bg-slate-400 h-screen'>
           <Toaster/>
