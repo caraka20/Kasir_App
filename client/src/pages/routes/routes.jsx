1f91bc3fed80e6c940d2cfd1b04099b33697ba57
rafansaoktaviano <ravansaoktaviano@gmail.com>
2023-09-21 17:07:25 +0700

    test

--- client/src/pages/routes/routes.jsx
+++ client/src/pages/routes/routes.jsx
@@ -1,7 +1,7 @@
 import { Route } from "react-router-dom";
 import CreatePorduk from "../CreateProduk/CreatePorduk";
 import Home from "../Home/Home";
-import Login from "../Login/Login"
+import Login from "../Login/Login";
 import ForgetPassword from "../Forget/ForgetPassword";
 import UpdatePassword from "../UpdatePassword/UpdatePassword";
 import Profile from "../Profile/Profile";
@@ -9,24 +9,17 @@ import Profile from "../Profile/Profile";
 import CreateCategory from "../CreateCategory/CreateCategory";
 import Admin from "../HomeAdmin/Admin";
 const routes = [
-  <> 
-    <Route path="/home/admin" element={<Admin />}/>
-    <Route path="/Category" element={<CreateCategory/>}/>
-
-
-// PAGES
-const routes = [
   <>
-
-    <Route path="/produk" element={<CreatePorduk />}/>
-    <Route path="/" element={<Home/>}/>
-    <Route path="/" element={<Login/>}/>
-    <Route path="/forgetpassword" element={<ForgetPassword/>}/>
-    <Route path="/updatepassword/:email" element={<UpdatePassword/>}/>
-    <Route path="/cashier"  element={<Home/>}/>
-    <Route path="/cashier/profile"  element={<Profile/>}/>
-  </>
-  
+    <Route path="/home/admin" element={<Admin />} />
+    <Route path="/Category" element={<CreateCategory />} />
+    <Route path="/produk" element={<CreatePorduk />} />
+    <Route path="/" element={<Home />} />
+    <Route path="/" element={<Login />} />
+    <Route path="/forgetpassword" element={<ForgetPassword />} />
+    <Route path="/updatepassword/:email" element={<UpdatePassword />} />
+    <Route path="/cashier" element={<Home />} />
+    <Route path="/cashier/profile" element={<Profile />} />
+  </>,
 ];
 
-export default routes
+export default routes;
