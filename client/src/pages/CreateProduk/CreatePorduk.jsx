152ac3773afd89d269202c33108cf67265774242
caraka wijaya <109886433+caraka20@users.noreply.github.com>
2023-09-23 13:04:16 +0700

    test merge

--- client/src/pages/CreateProduk/CreatePorduk.jsx
+++ client/src/pages/CreateProduk/CreatePorduk.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import LeftSideBarAdmin from '../../components/LeftSideBarAdmin/LeftSideBarAdmin'
-
+// import Sidebar from '../../components/LeftSideBarAdmin/Sidebar'
 import Button from '../../components/Button/Button'
 import axios from 'axios'
 import toast, {Toaster} from 'react-hot-toast'
+// import Sidebar from '../../components/LeftSideBarAdmin/Sidebar'
 
-const CreatePorduk = () => {
+
+    const CreatePorduk = () => {
     const [input, setInput] = useState({
         nama_produk:"",
         deskripsi:"",
@@ -13,6 +15,7 @@ const CreatePorduk = () => {
         harga:"",
         kategori_produk_id:""
     })
+
     const [datas, setDatas] = useState(null)
     const [images, setImages] = useState([])
 
@@ -22,7 +25,6 @@ const CreatePorduk = () => {
                 setDatas(getData)
                 console.log(getData.data.data);
         } catch (error) {
-            console.log(error);
             toast.error(error)
         }
     }
@@ -36,7 +38,6 @@ const CreatePorduk = () => {
 
             setImages(files)
         } catch (error) {
-            console.log(error);
             alert(error.message)
         }
     }
@@ -48,7 +49,7 @@ const CreatePorduk = () => {
             images.forEach(value => {
                 fd.append('images', value)
             })
-            console.log(fd);
+
             if(input.nama_produk === "" || input.deskripsi === "" || input.harga === "" || input.stock === "" || input.kategori_produk_id === "" || images.length === 0) {
                 return toast.error("Form Harus Dilengkapi")
             }
@@ -69,12 +70,10 @@ const CreatePorduk = () => {
                     toast.error("Produk Already Exist")
                 } 
                 const create = await axios.post("http://localhost:3001/product", fd)
-
-                console.log(create.data.message);
-                toast.success(create.data.message)
-
+                console.log(create.data);
+                // toast.success(create)
         } catch (error) {
-            console.log(error);
+            console.log(error.message);
             // toast.error(error)
         }
     }
@@ -93,15 +92,26 @@ const CreatePorduk = () => {
         getData()
     }, [])
 
+    // console.log();
+
+
   return (
     <div className='grid h-screen'>
         <Toaster />
         <div className='flex gap-3'>
             <LeftSideBarAdmin />
+
             <div className='w-full md:w-[90%] border h-full bg-blue-100'>
                 <div className='lg:p-10'>
 
                     <div className='flex justify-center text-5xl items-center font-semibold border-b-[5px] border-black py-5 mb-10'>
+
+
+            <div className='w-full md:w-[90%] border h-full bg-blue-100'>
+                <div className='lg:p-10'>
+
+                    <div className=' flex justify-center text-5xl items-center font-semibold border-b-[5px] border-black py-5 mb-10'>
+
                        <div>
                         Create Product
                        </div>
@@ -109,8 +119,13 @@ const CreatePorduk = () => {
 
                     {/* Form Input Create Product */}
                         <div className='bg-base-200 shadow-xl rounded-md'>
+
                             <div className='grid grid-col-2  align-middle'>
                                 <div className='grid w-[90%] justify-center mr-10 lg:grid-cols-2 mx-auto mt-5 p-4 pl-8'>
+
+                            <div className='grid align-middle'>
+                                <div className='flex justify-between lg:grid-cols-2 mx-auto mt-5 p-4 pl-8'>
+
                                     
                                     <div>
                                         <label htmlFor="" className='font-serif'>Nama Produk</label><br />
@@ -119,7 +134,7 @@ const CreatePorduk = () => {
 
                                     <div>
                                         <label htmlFor="" className='font-serif'>Pilih Kategori</label><br />
-                                        <select name='kategori_produk_id' onChange={handleChange} className='select w-full max-w-xs mt-2 mb-5 ' style={{ width: '100%' }}>
+                                        <select name='kategori_produk_id' onChange={handleChange} className='select w-full max-w-xs mt-2 mb-5' style={{ width: '100%' }}>
                                             <option disabled selected>Kategori Produk</option>
                                             <option value={1}>Snack</option>
                                             <option value={2}>Main Course</option>
@@ -135,7 +150,11 @@ const CreatePorduk = () => {
 
                                     <div>
                                         <label htmlFor="" className='font-serif'>Price</label><br />
+
                                         <input name='harga' value={input.harga} onChange={handleChange} type="text" placeholder='Type here' className='mt-2 mb-5 input input-bordered w-full max-w-xs'/>
+
+                                        <input type="text" placeholder='Type here' className='mt-2 mb-5 input input-bordered w-full max-w-xs'/>
+
                                     </div>
 
                                         <div className=''>
@@ -145,12 +164,21 @@ const CreatePorduk = () => {
 
                                         <div className='mt-[30px]'>
                                             <label htmlFor="" className="font-serif">Gambar Hotel</label><br />
+
                                             <input type='file' multiple='multiple' onChange={(e) => onSelectImages(e)} className="mt-2 mb-5 file-input file-input-bordered w-full max-w-xs bg-white" />
                                         </div>     
                                         
                                         <div></div>
                                         <div className='flex justify-start w-[75%]'>
                                             <Button onClick={createProduk} btnName="Submit" btnCSS="md:w-[50%] md:ml-[120px]"/> 
+
+                                            <input type="file" className="mt-2 mb-5 file-input file-input-bordered w-full max-w-xs bg-white" />
+                                        </div>     
+                                        
+                                        <div></div>
+                                        <div className='flex justify-start'>
+                                            <Button btnName="Submit" btnCSS="w-[35%] ml-0 md:ml-[120px]"/> 
+
                                         </div>                            
                                     </div>
                                          
