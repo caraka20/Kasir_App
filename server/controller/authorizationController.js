9df5cb739fe2f177518d8c1211c32cb15e83b37a
Arief Rubani <hidayat.arief44@yahoo.com>
2023-09-23 13:05:58 +0700

    MP3-23 backend 100%, frontend tinggal styling

--- server/controller/authorizationController.js
+++ server/controller/authorizationController.js
@@ -6,7 +6,6 @@ const { log } = require("util")
 const fs = require ('fs').promises
 
 module.exports = {
-    //LALAAYEYEYEYELALALAYEYEYE
     mailForgetPassword: async (req, res, next) => {
         try {
             const {email} = req.query
